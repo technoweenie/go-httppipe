fba4188c48b8cdcc59c6a3bee6710a46c71946a5
risk danger olson <technoweenie@gmail.com>
2013-10-20 08:08:33 -0600

    initial experiment

---
+++ httppipe.go
@@ -0,0 +1,42 @@
+package httppipe
+
+import (
+  "net/http"
+)
+
+type Pipe struct {
+  Handlers []http.Handler
+}
+
+func New(handlers []http.Handler) *Pipe {
+  return &Pipe{handlers}
+}
+
+func (p *Pipe) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  pipewriter := &PipeWriter{false, w}
+  for _, handler := range p.Handlers {
+    handler.ServeHTTP(pipewriter, r)
+    if pipewriter.written {
+      return
+    }
+  }
+
+  if !pipewriter.written {
+    p.serveHTTP(w, r)
+  }
+}
+
+func (p *Pipe) serveHTTP(w http.ResponseWriter, r *http.Request) {
+  w.WriteHeader(http.StatusInternalServerError)
+  w.Write([]byte(":("))
+}
+
+type PipeWriter struct {
+  written bool
+  http.ResponseWriter
+}
+
+func (w *PipeWriter) WriteHeader(status int) {
+  w.written = true
+  w.ResponseWriter.WriteHeader(status)
+}
---
+++ httppipe_test.go
@@ -0,0 +1,74 @@
+package httppipe
+
+import (
+  "io/ioutil"
+  "net/http"
+  "net/http/httptest"
+  "testing"
+)
+
+func TestPipe(t *testing.T) {
+  setup := Setup(t)
+  defer setup.Teardown()
+  
+  p := New([]http.Handler{
+    http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+      w.Header().Set("A", "1")
+    }),
+    http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+      w.Header().Set("B", "2")
+    }),
+    setup.Handler(),
+  })
+  
+  res := setup.Call(p)
+  if res.StatusCode != 200 {
+    t.Errorf("Bad response status: %d", res.StatusCode)
+  }
+  
+  if v := res.Header.Get("A"); v != "1" {
+    t.Errorf("Bad A value: %s", v)
+  }
+  
+  if v := res.Header.Get("B"); v != "2" {
+    t.Errorf("Bad B value: %s", v)
+  }
+  
+  by, _ := ioutil.ReadAll(res.Body)
+  if string(by) != "OK" {
+    t.Errorf("Bad response body: %s", string(by))
+  }
+}
+
+type PipeSetup struct {
+	Server *httptest.Server
+	Mux    *http.ServeMux
+  t *testing.T
+}
+
+func Setup(t *testing.T) *PipeSetup {
+	mux := http.NewServeMux()
+	srv := httptest.NewServer(mux)
+	return &PipeSetup{srv, mux, t}
+}
+
+func (s *PipeSetup) Teardown() {
+	s.Server.Close()
+}
+
+func (s *PipeSetup) Call(p *Pipe) *http.Response {
+  s.Mux.Handle("/", p)
+  res, err := http.Get(s.Server.URL + "/")
+  if err != nil {
+    s.t.Fatalf("Error making request: %s", err)
+  }
+  return res
+}
+
+func (s *PipeSetup) Handler() http.Handler {
+  f := func(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("OK"))
+  }
+  return http.HandlerFunc(f)
+}
