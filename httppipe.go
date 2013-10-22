ec0285414a1f254356d6623ba57f30f126dd17c1
rick <technoweenie@gmail.com>
2013-10-22 09:17:46 -0700

    docs, and hide pipeWriter

--- httppipe.go
+++ httppipe.go
@@ -1,22 +1,27 @@
+// Package httppipe contains an http.Handler that runs a series of http.Handler
+// filters until one of them writes a response.
 package httppipe
 
 import (
 	"net/http"
 )
 
+// A Pipe is a collection of http.Handler filters to run.
 type Pipe struct {
 	Handlers []http.Handler
 	Fallback http.HandlerFunc
 }
 
+// New initializes a Pipe with the given handlers.
 func New(handlers []http.Handler) *Pipe {
 	p := &Pipe{Handlers: handlers}
 	p.Fallback = p.serveHTTP
 	return p
 }
 
+// ServeHTTP satisfies the http.Handler interface.
 func (p *Pipe) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	pipewriter := &PipeWriter{false, w}
+	pipewriter := &pipeWriter{false, w}
 	for _, handler := range p.Handlers {
 		if handler == nil {
 			continue
@@ -38,12 +43,12 @@ func (p *Pipe) serveHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(":("))
 }
 
-type PipeWriter struct {
+type pipeWriter struct {
 	written bool
 	http.ResponseWriter
 }
 
-func (w *PipeWriter) WriteHeader(status int) {
+func (w *pipeWriter) WriteHeader(status int) {
 	w.written = true
 	w.ResponseWriter.WriteHeader(status)
 }
